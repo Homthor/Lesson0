6237a1e5bd8fd3135c2fdd82774c778d0ad381b9
Homthor <157229322+Homthor@users.noreply.github.com>
2024-11-10 16:08:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# 1st program
+ans1 = 9**0.5*5
+print(ans1)
+
+# 2nd program
+
+ans2 = 9.99 > 9.98 and 9.99 != 1000.1
+print(ans2)
+
+# 3rd program
+
+ans3 = 2 * 2 + 2
+ans4 = 2 * (2 + 2)
+
+#(ans3, ans4)
+
+print(ans3 == ans4)
+
+# 4th program
+
+ans5 = '123.456'
+ans5 = float(ans5)
+ans5 = ans5 * 10 % 10
+ans5 = int(ans5)
+
+print(ans5)
